9d4a3acb881439fc7aa8765c1ab8eb1455e165be
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-01-30 06:37:28 -0800

    Hair movement

---
+++ Assets/_GGJ16/Scripts/HairJoint.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HairJoint : MonoBehaviour {
+
+	public int index;
+	public Transform prevJoint;
+
+	public float spacing;
+	public float range;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		range = 0.5f;
+		speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (prevJoint != null ||
+			index > 0) {
+			MoveMe ();
+		}
+	//	if (this.index == 0) {
+			transform.Rotate (0, Mathf.Sin (Time.time*speed)*range, 0);
+
+	//	}
+	}
+
+	void MoveMe(){
+		transform.LookAt (prevJoint);
+		transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (prevJoint.forward), 10*Time.deltaTime);
+		Vector3 desiredPos = prevJoint.position - (transform.forward.normalized * spacing);
+		transform.position = Vector3.Lerp (transform.position, desiredPos, 10*Time.deltaTime);
+		//(prevJoint.forward*-1) + transform.forward.normalized * spacing;
+		//			transform.position = desiredPos;
+		//transform.position = desiredPos;
+	}
+}
---
+++ Assets/_GGJ16/Scripts/HairManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HairManager : MonoBehaviour {
+
+	int indexer;
+	public string hairJointPrefix;
+	IList<Transform> joints;
+
+	public bool init;
+	// Use this for initialization
+	void Start () {
+		indexer = 0;
+		joints = new List<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (init) {
+			FindAllJoints (hairJointPrefix);
+			init = false;
+		}
+	}
+	void FindAllJoints(string jointName){
+		Transform[] children = gameObject.transform.GetComponentsInChildren<Transform> ();
+		for(int i = 0; i < children.Length; i++){
+
+			Transform puppet = children[i];
+			if (puppet.name.Contains (jointName)) {
+				Debug.Log ("found");
+				HairJoint joint = puppet.gameObject.AddComponent<HairJoint> () as HairJoint;
+
+				joint.index = indexer;
+
+				if (indexer > 0) {
+					joint.prevJoint = joints [indexer - 1];
+					joint.spacing = Vector3.Distance (puppet.position, joint.prevJoint.position);
+				}
+				indexer++;
+				joints.Add (puppet);
+			}
+		}
+	}
+}
