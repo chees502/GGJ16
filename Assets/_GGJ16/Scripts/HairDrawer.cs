47141dbaa0f34dc1f8d8a2ef6de4dae80b055b6f
brandon <chees502@gmail.com>
2016-01-30 05:14:48 -0800

    Player Controller with Buck, and serpantspace

---
+++ Assets/_GGJ16/Scripts/HairDrawer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HairDrawer : MonoBehaviour {
+    LineRenderer lr;
+    int index;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lr.SetPosition(index, transform.position);
+	}
+    public void SetValues(LineRenderer lr, int index)
+    {
+        this.lr = lr;
+        lr.SetWidth(0.1f, 0.1f);
+        this.index = index;
+    }
+}
--- Assets/_GGJ16/Scripts/PlayerController.cs
+++ Assets/_GGJ16/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
     public float speed = 1;
     public PlayerNumber number;
+    public float slideAmount = 0.1f;
+    public Vector3 lateral = Vector3.right;
+    public float dot = 0;
     void Update(){
         Move(GetDir());
+        Buck(transform.position.x);
     }
     public void Move(Vector3 dir){
         transform.position += dir*speed*Time.deltaTime;
@@ -15,5 +19,11 @@ public class PlayerController : MonoBehaviour {
         dir.x = Input.GetAxis(number.ToString() + "x");
         dir.z = -Input.GetAxis(number.ToString() + "y");
         return dir;
+    }
+    public void Buck(float latPos)
+    {
+        latPos = Mathf.Clamp(latPos, -1, 1);
+        float amount = latPos * Mathf.Abs(dot)+dot;
+        transform.position += lateral * amount*Time.deltaTime;
     }
 }
