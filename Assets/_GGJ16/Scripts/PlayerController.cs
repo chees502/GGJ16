a92a8d8d52678e8cc72d60d585312d7e217c5d59
brandon <chees502@gmail.com>
2016-01-30 02:38:51 -0800

    added basic controller for player, and dealt with input manager.

---
+++ Assets/_GGJ16/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1;
+    public PlayerNumber number;
+    void Update(){
+        Move(GetDir());
+    }
+    public void Move(Vector3 dir){
+        transform.position += dir*speed*Time.deltaTime;
+    }
+    public Vector3 GetDir(){
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.GetAxis(number.ToString() + "x");
+        dir.z = -Input.GetAxis(number.ToString() + "y");
+        return dir;
+    }
+}
---
+++ Assets/_GGJ16/Scripts/PlayerNumber.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerNumber
+{
+    Player1,
+    Player2,
+    Player3,
+    Player4
+}
