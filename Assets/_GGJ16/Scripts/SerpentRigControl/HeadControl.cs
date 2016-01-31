5e3627ce073aee28c5f0a05702304bdad17a5e50
brandon <chees502@gmail.com>
2016-01-30 16:49:58 -0800

    AI!

--- Assets/_GGJ16/Scripts/SerpentRigControl/HeadControl.cs
+++ Assets/_GGJ16/Scripts/SerpentRigControl/HeadControl.cs
@@ -4,18 +4,29 @@ using System.Collections;
 namespace Serpent{
 	public class HeadControl : MonoBehaviour {
         public static HeadControl main;
-
+        float seed;
         public float speed = 15;
         public float turnRadius = 150;
 		// Use this for initialization
 		void Awake () {
             main = this;
+            seed = Random.Range(0.0f, 8000.0f);
 		}
 		
 		// Update is called once per frame
 		void Update () {
             transform.position += transform.forward*Time.deltaTime*speed;
+            if (_Root.flyingPlayer == null)
+            {
+                AI();
+            }
 		}
+        void AI()
+        {
+            float deg = Mathf.PerlinNoise(0, seed+Time.time*0.4f)*Mathf.PI*2;
+            Vector3 dir = new Vector3(Mathf.Sin(deg), 0, -Mathf.Cos(deg));
+            Move(dir);
+        }
         public void Move (Vector3 dir){
             if (dir.magnitude < 0.1f) return;
             dir.Normalize();
