d559ad09b85cdc51a365ff8fd56e528e426a180a
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-01-30 17:41:35 -0800

    solved for serpent body rig movement

--- Assets/_GGJ16/Scripts/SerpentRigControl/HeadControl.cs
+++ Assets/_GGJ16/Scripts/SerpentRigControl/HeadControl.cs
@@ -4,6 +4,9 @@ using System.Collections;
 namespace Serpent{
 	public class HeadControl : MonoBehaviour {
 
+		void Awake(){
+		//	transform.Rotate (0, 180, 0);
+		}
 		// Use this for initialization
 		void Start () {
 		
--- Assets/_GGJ16/Scripts/SerpentRigControl/JointControl.cs
+++ Assets/_GGJ16/Scripts/SerpentRigControl/JointControl.cs
@@ -13,42 +13,55 @@ namespace Serpent{
 		public float range;
 		public float speed;
 		public float jointRotateSpeed;
-
+		public bool is_x_forward;
+		public bool isHeadInit;
 		// Use this for initialization
+		void Awake(){
+			
+		}
+
 		void Start () {
 			range = 0.5f;
 			speed = 3;
-
+			if (index == 0) {
+				puppetJoint.Rotate (180, 180, 0);
+			}
 		}
 		
 		// Update is called once per frame
 		void Update () {
-			puppetJoint.position = transform.position;
+		//	puppetJoint.position = transform.position;
+		//	puppetJoint.rotation = transform.rotation;
 			if (prevJoint != null ||
 			   index > 0) {
 				MoveMe ();
-				puppetJoint.rotation = transform.rotation * rotOffset;//Quaternion.Euler (0, 180, 0);// (-90, -90, 0);
+			//	puppetJoint.rotation = transform.rotation * rotOffset;//Quaternion.Euler (0, 180, 0);// (-90, -90, 0);
+			//	puppetJoint.localRotation = transform.rotation * rotOffset;
 			}
 			if (this.index == 0) {
-				transform.Rotate (0, 0,Mathf.Sin (Time.time*speed)*range);
+				transform.Rotate (0,Mathf.Sin (Time.time*speed)*range, 0);
 
 			}
 
 		}
 
 		public void SetOffset(Transform puppet){
-			rotOffset = Quaternion.FromToRotation (transform.forward, transform.up);
+		//	rotOffset = Quaternion.FromToRotation(transform.forward, transform.up);
+			rotOffset = Quaternion.Euler(90,0,-90);
+			puppet.transform.parent = null;
+			puppet.transform.parent = transform;
 			Debug.Log (rotOffset);
 		}
 		void MoveMe(){
-			transform.LookAt (prevJoint,Vector3.forward);
+			transform.LookAt (prevJoint);
 			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (prevJoint.forward), jointRotateSpeed * Time.deltaTime);
 			Vector3 desiredPos = prevJoint.position - (transform.forward.normalized * spacing);
 			//transform.position = Vector3.Lerp (transform.position, desiredPos, Time.deltaTime);
 				//(prevJoint.forward*-1) + transform.forward.normalized * spacing;
 //			transform.position = desiredPos;
 			transform.position = desiredPos; 
-			LineDebug (puppetJoint);
+		//	LineDebug (puppetJoint);
+
 		}
 
 		void LineDebug(Transform pos){
